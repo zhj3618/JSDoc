79fe74beef6dcf89853b3d530fc8e22b166f11d7
Michael Mathews <micmath@gmail.com>
2010-03-28 20:12:08 +0100

    Added more tests for quoted name.

--- test/jsdoc/quotedName_test.js
+++ test/jsdoc/quotedName_test.js
@@ -10,33 +10,45 @@ exports.testQuotedNameInTag = function () {
 }
 
 exports.testQuotedMethodInTag = function () {
-	assertEqual(docSet.getDocByName('Foo#"=b@r#;"').name, 'Foo#"=b@r#;"');
-	assertEqual(docSet.getDocByName('Foo#"=b@r#;"').memberof, 'Foo#');
-	assertEqual(docSet.getDocByName('Foo#"=b@r#;"').shortname, '"=b@r#;"');
+	var doc = docSet.getDocByName('Foo#"=b@r#;"');
+	assertEqual(doc.name, 'Foo#"=b@r#;"');
+	assertEqual(doc.memberof, 'Foo#');
+	assertEqual(doc.shortname, '"=b@r#;"');
 }
 
 exports.testNestedQuotedMethodInTag = function () {
-	assertEqual(docSet.getDocByName('Foo."=b@r#;"#fip').name, 'Foo."=b@r#;"#fip');
-	assertEqual(docSet.getDocByName('Foo."=b@r#;"#fip').memberof, 'Foo."=b@r#;"#');
-	assertEqual(docSet.getDocByName('Foo."=b@r#;"#fip').shortname, 'fip');
+	var doc = docSet.getDocByName('Foo."=b@r#;"#fip');
+	assertEqual(doc.name, 'Foo."=b@r#;"#fip');
+	assertEqual(doc.memberof, 'Foo."=b@r#;"#');
+	assertEqual(doc.shortname, 'fip');
 }
 
-exports.testQuotedEscapedMethodInTag = function () {
-	assertEqual(docSet.getDocByName('Foo#"it\'s \\"bar!\\""').name, 'Foo#"it\'s \\"bar!\\""');
-	assertEqual(docSet.getDocByName('Foo#"it\'s \\"bar!\\""').memberof, 'Foo#');
-	assertEqual(docSet.getDocByName('Foo#"it\'s \\"bar!\\""').shortname, '"it\'s \\"bar!\\""');
+exports.testEscapedQuotedMethodInTag = function () {
+	var doc = docSet.getDocByName('Foo#"it\'s \\"bar!\\""');
+	assertEqual(doc.name, 'Foo#"it\'s \\"bar!\\""');
+	assertEqual(doc.memberof, 'Foo#');
+	assertEqual(doc.shortname, '"it\'s \\"bar!\\""');
 }
 
 exports.testStrangeQuotedMethodInTag = function () {
-	assertEqual(docSet.getDocByName('Foo."@name Foo.\\"@name"#name').name, 'Foo."@name Foo.\\"@name"#name');
-	assertEqual(docSet.getDocByName('Foo."@name Foo.\\"@name"#name').memberof, 'Foo."@name Foo.\\"@name"#');
-	assertEqual(docSet.getDocByName('Foo."@name Foo.\\"@name"#name').shortname, 'name');
+	var doc = docSet.getDocByName('Foo."@name Foo.\\"@name"#name');
+	assertEqual(doc.name, 'Foo."@name Foo.\\"@name"#name');
+	assertEqual(doc.memberof, 'Foo."@name Foo.\\"@name"#');
+	assertEqual(doc.shortname, 'name');
 }
 
 exports.testPrivateQuotedMethodInTag = function () {
 	var doc = docSet.getDocByName('"F-o.o#o"-foz');
-	assertEqual(doc.name, '"F-o.o#o"');
+	assertEqual(doc.name, '"F-o.o#o"-foz');
 	assertEqual(doc.memberof, '"F-o.o#o"');
 	assertEqual(doc.shortname, 'foz');
 	assertEqual(doc.isinner, true);
+}
+
+exports.testStaticQuotedMethodInTag = function () {
+	var doc = docSet.getDocByName('"F-o.o#o".fez');
+	assertEqual(doc.name, '"F-o.o#o".fez');
+	assertEqual(doc.memberof, '"F-o.o#o"');
+	assertEqual(doc.shortname, 'fez');
+	assertEqual(doc.isstatic, true);
 }
